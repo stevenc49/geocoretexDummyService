a39207dba2cbcb6cc56d0010b75f1eb9cc17d189
Steven Cheong <stevenc49@gmail.com>
2014-10-09 23:39:49 -0700

    socket.io timer working

--- index.js
+++ index.js
@@ -27,9 +27,8 @@ app.get('/workflows/good.json', function(req, res){
 //////////////////////////////////////
 io.on('connection', function(socket){
     console.log('a connection was established');
-    socket.emit('update', 'express1:OK');
+    socket.emit('connected', 'you are now connected to the socket.io server');
 });
-
 
 app.get('/socketsend', function(req, res){
   res.sendFile('/workflows/good.json', { root: __dirname });
@@ -38,3 +37,17 @@ app.get('/socketsend', function(req, res){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+//////////////////////////////////////
+//     timer
+//////////////////////////////////////
+function checkAllWorkflows() {
+    var sleepTime = 1000;
+
+    console.log("Checking all workflows...");
+
+    io.sockets.emit('broadcast', 'workflow updates');
+    setTimeout(function () { checkAllWorkflows() }, sleepTime);
+}
+
+checkAllWorkflows();
