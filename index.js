0e8f03c15bc56d931647cb846a1fab25b6d93100
Steven Cheong <stevenc49@gmail.com>
2014-10-10 00:52:58 -0700

    calling workflow and sending response to frontend working

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
 app.get('/', function(req, res){
   res.sendFile('index.html', { root: __dirname });
 });
 
 //////////////////////////////////////
-//     mock geocortex workflows
+//     geocortex mock workflows
 //////////////////////////////////////
 
 app.get('/workflows/error.json', function(req, res){
@@ -30,22 +34,46 @@ io.on('connection', function(socket){
     socket.emit('connected', 'you are now connected to the socket.io server');
 });
 
-app.get('/socketsend', function(req, res){
-  res.sendFile('/workflows/good.json', { root: __dirname });
-});
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
 //////////////////////////////////////
 //     timer
 //////////////////////////////////////
 function checkAllWorkflows() {
     var sleepTime = 1000;
 
-    console.log("Checking all workflows...");
+    var url = "http://localhost:8888/monitorServices/pingUrl?url=http://www.google.ca";
+    console.log("Calling " + url);
+    
+    require('http').get(url, function(res) { 
+      
+      res.on('data', function (chunk) {
+        console.log('Response: ' + chunk);
 
+        io.sockets.emit('broadcast', chunk.toString());
+      });
+    }).on('error', function(e) {
+      console.log("Got error: " + e.message);
+    });
+
+    /*
+    var options = {
+      hostname: 'localhost',
+      port: 8888,
+      path: '/pingUrl?url=http://www.google.ca',
+      method: 'GET'
+    };
+    
+    var req = require('http').request(options, function(res) {
+      console.log('STATUS: ' + res.statusCode);
+      console.log('HEADERS: ' + JSON.stringify(res.headers));
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        console.log('BODY: ' + chunk);
+      });
+    });
+    
+    console.log(req);
+    */
+    
     io.sockets.emit('broadcast', 'workflow updates');
     setTimeout(function () { checkAllWorkflows() }, sleepTime);
 }
